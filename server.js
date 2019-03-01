966838b468c365df3b5a0795473f1da8d226b8a6
ilya <ilya.levkov1992@gmial.com>
2019-03-01 18:18:37 +0200

    remove node modules and users route

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/users', (req, res) => {
+    const users = [
+        {id: 1, firstName:'John', lastName:'Dou'},
+        {id: 2, firstName:'Steve', lastName:'Smith'}
+    ];
+    res.json(users)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
