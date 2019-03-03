791379fb6f6c8c6d7952fb2aa15fa3478e808f8b
levcovic <jundev@gmail.com>
2019-03-03 23:50:17 +0200

    articles model, mongoose, articles route

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const express = require('express');
-const app = express();
 const port = 8080;
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yes_db', { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', function() {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', function(err) {
+    console.log(err);
+});
+
+const app = express();
+
+let Article = require('./models/article');
 
 app.get('/api/users', (req, res) => {
     const users = [
         {id: 1, firstName:'John', lastName:'Dou'},
         {id: 2, firstName:'Steve', lastName:'Smith'}
     ];
-    res.json(users)
+    res.json(users);
+});
+
+app.get('/api/articles', (req, res) => {
+    Article.find({}, function(err, articles) {
+        res.json(articles);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
